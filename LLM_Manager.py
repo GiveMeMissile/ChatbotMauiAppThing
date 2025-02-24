36be47f7463bd8586ea51a7717dbcbe9ab5e9029
GiveMeMissile <148016515+GiveMeMissile@users.noreply.github.com>
2025-02-23 20:54:28 -0600

    Add files via upload

---
+++ LLM_Manager.py
@@ -0,0 +1,38 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig  # MUHAHAHA UwU
+
+
+TOKENIZER = "Qwen/Qwen2.5-1.5B-Instruct"
+MODEL = "Qwen/Qwen2.5-1.5B-Instruct"
+MAX_TOKENS = 512
+CONTEXT = "An AI assistant is assisting a user. The AI model is Qwen2.5 1.5b. The user is the user of an app called 'The AI helpline'. It is a very good app. The conversation between the AI and User is seen below."
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+class LLMManager:
+    def __init__(self):
+        config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_compute_dtype=torch.float16
+        )
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            TOKENIZER
+        )
+        self.model = AutoModelForCausalLM.from_pretrained(
+            MODEL,
+            quantization_config=config,
+            device_map="auto"
+        )
+    
+    def forward(self, text):
+        input_text = CONTEXT + text
+        tokens = self.tokenizer(input_text, return_tensors="pt")["input_ids"].to(device)
+        logits = self.model(tokens, attention_mask=None).logits[:, -1, :]
+        prob = torch.argmax(logits, dim=-1).type(torch.int64)
+        output = self.tokenizer.decode(prob.item())
+        if self.tokenizer.eos_token_id == prob.item() or output == "User" or output == "<|endoftext|>":
+
+            # Best return statement ever.
+            return "<|EndGenerationNowUWU|>"
+        else:
+            return output
---
+++ MainPage.xaml.cs
@@ -0,0 +1,95 @@
+﻿using Python.Runtime;
+using System.Threading.Tasks;
+using Microsoft.Maui.ApplicationModel;
+
+namespace ChatbotThingyWow;
+
+public partial class MainPage : ContentPage
+{
+    string aiInput = "";
+	bool generating = false;
+	dynamic aiManager;
+	string endGeneration = "<|EndGenerationNowUWU|>";
+
+
+    private async Task GenerateResponce()
+    {
+        // Set up a new label to output the AI's response
+        Label label = new Label();
+        label.Text = "";
+        AIUserChat.Add(label);
+
+        // Change the User's editor so they cannot input anything (:)
+        //UserInputEditor.IsReadOnly = true;
+        //UserInputEditor.Text = "";
+        //UserInputEditor.Placeholder = "Generating...";
+        await MainThread.InvokeOnMainThreadAsync(() => {
+            AIUserChat.Add(label);
+            UserInputEditor.IsReadOnly = true;
+            UserInputEditor.Text = "";
+            UserInputEditor.Placeholder = "Generating...";
+        });
+
+
+        string text;
+            
+        while (generating)
+        {
+            using (Py.GIL())
+            {
+                //text = (string)aiManager.forward(aiInput);
+                text = "UWU";
+                Random random = new Random();
+                if (random.Next(1, 100) == 42)
+                {
+                    text = endGeneration;
+                }
+            }
+
+            if (text == endGeneration)
+            {
+                await MainThread.InvokeOnMainThreadAsync(() => {
+                    UserInputEditor.IsReadOnly = false;
+                    UserInputEditor.Placeholder = "Ask the AI assistant anything you want!";
+                });
+                //UserInputEditor.IsReadOnly = false;
+                //UserInputEditor.Placeholder = "Ask the AI assistant anything you want!";
+                break;
+            }
+            await MainThread.InvokeOnMainThreadAsync(() => {
+                label.Text += text;
+            });
+            //label.Text += text;
+            aiInput += text;
+                
+            await Task.Delay(10);
+        }
+    }
+
+    private void InputText(object sender, EventArgs e)
+    {
+        Label label = new Label();
+        label.Text = "\nUser:\n" + UserInputEditor.Text;
+        aiInput += label.Text;
+        AIUserChat.Add(label);
+
+        Task.Run(async () => await GenerateResponce());
+    }
+
+    public MainPage()
+    {
+        InitializeComponent();
+        Runtime.PythonDLL = @"C:\Users\dylan\anaconda3\python312.dll";
+        PythonEngine.Initialize();
+        using (Py.GIL())
+        {
+            dynamic sys = Py.Import("sys");
+            sys.path.append(@"ChatbotThingyWow");  // Fixed path.append
+
+            aiManager = Py.Import("LLM_Manager");
+			aiManager = aiManager.LLMManager();
+        }
+        
+        // Task.Run(async () => await GenerateResponce());
+    }
+}
